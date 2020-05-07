a6632bc7790e13d4690ba00933d5c90729f2da31
EliseR101 <elise.rattigan@googlemail.com>
2020-05-07 12:41:46 +0800

    Up to Chapter 6 of Flask Mega Tutorial

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,6 +4,8 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+# Chap 6
+from hashlib import md5
 
 #UserMixin from Chap 5
 class User(UserMixin, db.Model):
@@ -12,7 +14,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # Chap 6 of Mega Tutorial
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # Chap 5
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -22,6 +28,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # Chap 6 of Mega Tutorial
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
