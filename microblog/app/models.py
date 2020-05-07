15ce3bea8e36fac88755a9a0b336835951e61e26
EliseR101 <elise.rattigan@googlemail.com>
2020-05-07 11:43:05 +0800

    Up to chapter 5 of Mega tutorial

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from app import db
+# Chap 5 of Mega Tutorial
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+#UserMixin from Chap 5
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +16,13 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    # Chapter 5 of Mega Tutorial
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -19,3 +31,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# Chap 5 of Mega Tutorial
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
