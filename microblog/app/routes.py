85cadfa9e486984a91a00ad4757dd169e5934146
EliseR101 <elise.rattigan@googlemail.com>
2020-05-13 18:28:58 +0800

    Up to Chap 9 of Flask Mega Tutorial

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -11,23 +11,38 @@ from app.forms import RegistrationForm
 # Chap 6
 from datetime import datetime
 from app.forms import EditProfileForm
+#Chap 9
+from app.forms import PostForm
+from app.models import Post
 
-@app.route('/')
-@app.route('/index')
+# Chap 9 - now accepting POST requests in both routes associated with index view as this 
+# view function will now receive form data 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 # Chap 5 of Mega Tutorial
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home Page', post=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        # inserts a new Post record into the database
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    # Chap 9 - pagination
+    page = request.args.get('page', 1, type=int)
+    # Chap 9 - fake posts replaced with real posts
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    # Chap 9 - template receives the form object as an additional argument, so that it can render the 
+    # text field
+    return render_template("index.html", title='Home', form=form, posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 #Chap 5 of Mega Tutorial
 @app.route('/login', methods=['GET', 'POST'])
@@ -73,11 +88,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    # Chap 9 - pagination
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 #Chap 6 of Mega Tutorial
 @app.before_request
@@ -132,3 +152,18 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+# Chap 9 of Mega Tutorial
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    # reusing the index.html template for this page, but not including the form to submit blog posts
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, 
+                            prev_url=prev_url)
--- microblog/config.py
+++ microblog/config.py
@@ -10,3 +10,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #Chap 9 of Mega Tutorial
+    POSTS_PER_PAGE = 25
