d415620cbe7b5bc711a1ffec76246068610e9daf
EliseR101 <elise.rattigan@googlemail.com>
2020-05-20 10:13:03 +0800

    Readding database migrations

---
+++ migrations/versions/769d1495747d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 769d1495747d
+Revises: 37e790ff23e1, c257cf5ce20b
+Create Date: 2020-05-19 10:06:58.639023
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '769d1495747d'
+down_revision = ('37e790ff23e1', 'c257cf5ce20b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
