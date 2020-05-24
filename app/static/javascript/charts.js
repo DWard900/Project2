4184db8fad05658df5a897ebb4be3d8631e0f766
Dan Ward <22744074@student.uwa.edu.au>
2020-05-24 17:33:21 +0800

    second graph

--- app/static/javascript/charts.js
+++ app/static/javascript/charts.js
@@ -133,3 +133,85 @@ function exerciseGroupGraph(arr) {
     });
 
 };
+
+
+function fastestRunData() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() { 
+        if (this.readyState == 4 && this.status == 200) {
+        const myData = JSON.parse(this.responseText);
+        fastestRunGraph(myData);
+        }
+    };
+    let url = "http://localhost:5000//api/users/all/exercise";
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+
+function fastestRunGraph(arr) {
+    let ctx = document.getElementById("chart2").getContext('2d');
+    let user = [];
+    let time = [];
+    let distance = [];
+    let speed = [];
+
+    for (x in arr) {
+        user.push(arr[x].username);
+        time.push(parseFloat(arr[x].time));
+        distance.push(parseFloat(arr[x].distance));
+    }
+    for (i=0;i<user.length;i++){
+        speed.push(time[i]/distance[i])
+    }
+
+    ///Creates a unique array of users
+    var uniqueusers = [];
+    var count = 0;
+    var found = false;
+
+    for (i=0;i<user.length;i++){
+        for (y = 0; y<uniqueusers.length; y++){
+            if(user[i] == uniqueusers[y]){
+                found = true;
+            }
+        }
+        count++;
+        if(count == 1 && found ==false){
+            uniqueusers.push(user[i]);
+        }
+        count=0;
+        found = false;
+    }
+
+    
+    
+
+    var barChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: user,
+            datasets: [{
+                label: "Mintues per KM",
+                data: speed,
+                backgroundColor: [
+                    "#331E36",
+                    "#A5FFD6",
+                    "#331E36",
+                    "#A5FFD6"
+                ],
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+        
+    });
+
+};
+       
