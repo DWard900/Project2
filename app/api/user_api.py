1ee91acffb2823274250923e545257872d6149d1
EliseR101 <elise.rattigan@googlemail.com>
2020-05-25 22:25:32 +0800

    Formatting

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -9,6 +9,7 @@ from app.api.auth import token_auth
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
 
+# API to get user exercise
 @app.route('/api/users/<int:userId>/exercise', methods=['GET'])
 ##@token_auth.login_required
 def get_exercise(userId):
@@ -23,6 +24,7 @@ def get_exercise(userId):
                         'exercise_comments': e.exercise_comments})
     return jsonify(exercise)
 
+# API to get all users
 @app.route('/api/users', methods=['GET', 'POST'])
 ##@token_auth.login_required
 def get_user_list():
@@ -33,7 +35,7 @@ def get_user_list():
                     'last_seen': u.last_seen.strftime("%A, %d %B %Y at %H:%M UTC"), 'exercise_count': u.exercise.count()})
     return jsonify(users)
 
-# API to get exercise for one user
+# API to get exercise for one user for graphs
 @app.route('/api/users/<int:userId>/exercise/graphs', methods=['GET'])
 def exercise_graph(userId):
     user = User.query.get_or_404(userId)
@@ -55,31 +57,3 @@ def exercise_all():
         for e in exercises:
             users.append({'id': u.id, 'username': u.username, 'style': e.style, 'distance':e.distance , 'time': e.time , 'date': e.exercise_date.strftime("%Y-%m-%d") })
     return jsonify(users)
-
-
-'''@app.route('/api/users', methods=['POST'])
-def register_user():
-    data = request.get_json() or {}
-    if 'id' not in data:
-        return bad_request('Must include id')
-    user = User.query.get(data['id'])
-    if user is None:
-        return bad_request('Unknown user')
-    if user.password_hash is not None:
-        return bad_request('User already registered')
-    user.from_dict(data)
-    db.session.commit()
-    response =jsonify(user.to_dict())
-    response.status_code = 201 #creating a new resource should chare the location....
-    response.headers['Location'] = url_for('get_user',id=user.id)
-    return response'''
-
-'''@app.route('/api/users/delete/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
-    if user is None:
-        return bad_request('Unknown user')
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify(user.to_dict())'''
-
